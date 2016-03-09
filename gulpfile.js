0883f2805742ca6d920c927561845727ec1578f6
Chaela <chawomack@gmail.com>
2016-03-09 15:36:43 -0500

    added gulp and database dump

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
+var childProcess = require('child_process');
+
+gulp.task('default', function(){
+  childProcess.exec('mongod', function(err, stdout, stderr){
+    gulpUtil.log(stdout);
+  });
+  childProcess.exec('mongorestore 6DegreesCRM', function(err, stdout, stderr){
+    gulpUtil.log(stdout);
+  });
+  childProcess.exec('npm start', function(err, stdout, stderr){
+    gulpUtil.log(stdout);
+  });
+});
