444fee8f861080f3cba68bfd1f96936177dbde1c
Michael Womack <mwomack93@gmail.com>
2016-04-26 16:45:33 -0400

    ordersCtrl calls Orders model functions

--- public/javascripts/angular-order.js
+++ public/javascripts/angular-order.js
@@ -1,8 +1,11 @@
 var app = angular.module('CRM');
 
-app.controller('ordersCtrl', ['$scope', 'Orders',
-    function ($scope, Orders) {
-        $scope.orders = [];
+app.controller('ordersCtrl', ['$scope', 'Orders', function ($scope, Orders) {
+        $scope.getAllOrders = Orders.getAll().then($scope.orders = Orders.data.orders);
+        $scope.getIncoming = Orders.getIncoming().then($scope.incomingOrders = Orders.data.orders);
+        $scope.confirmShipment = Orders.confirmShipment(order).then($scope.orders = Orders.data.orders);
     }]);
+
+
 
 
